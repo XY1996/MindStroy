c248defde1df2fd1a39f0e4606837605133c280b
Goxy <goxy@cognitiveleap.com>
2017-03-24 00:47:25 +0800

    ADD XMIND DLL

---
+++ Assets/Test/UnitTest/TestXmindAPI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XMindAPI.LIB;
+using System.IO;
+
+public partial class TestXmindAPI : MonoBehaviour
+{
+    const string path2 = "C:\\Users\\xy\\Desktop\\隐形玩家剧本（第3版）.xmind";
+    void Start()
+    {
+
+        XMindWorkBook xwb = new XMindWorkBook( path2, true);
+
+        // Read workbook content and format into a report containing parent and child by sheet:
+        List<XMindSheet> sheets = xwb.GetSheetInfo();
+
+
+
+    }
+
+
+}
+
