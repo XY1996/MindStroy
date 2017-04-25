5eeffed3a142c2c3f388d4cde87fcf8ef61756b2
Goxy <Goxy@cognitiveleap.com>
2017-04-26 00:24:24 +0800

    Update Editor

---
+++ Fungus/Assets/MindStory/Editor/FlowcharGenerateEditor.cs
@@ -0,0 +1,32 @@
+﻿using Fungus;
+using System.Collections;
+using System.Collections.Generic;
+using Fungus.EditorUtils;
+using UnityEditor;
+using UnityEngine;
+
+public class FlowcharGenerateEditor : Editor {
+
+    [ExecuteInEditMode]
+    [MenuItem("MindStory/Generate")]
+    public static void menu()
+    {
+        Flowchart flowchart = FindObjectOfType<Flowchart>();
+        if (flowchart == null)
+        {
+            GameObject go = new GameObject("Flowchart");
+            flowchart = go.AddComponent<Flowchart>();
+        }
+        else
+        {
+            DestroyImmediate(flowchart.gameObject);
+            GameObject go = new GameObject("Flowchart");
+            flowchart = go.AddComponent<Flowchart>();
+        }
+        FlowchartWindow.CreateBlock(flowchart, Vector2.zero);
+
+
+
+
+    }
+}
--- Xmind/FreeMinLib/MindInfo/FreeMindArrowLink.cs
+++ Xmind/FreeMinLib/MindInfo/FreeMindArrowLink.cs
@@ -1,9 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 
-
-
-[System.Serializable]
 public class FreeMindArrowLink : ArrowLinkBase
 {
 
